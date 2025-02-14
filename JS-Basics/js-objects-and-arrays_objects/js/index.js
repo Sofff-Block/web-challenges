8f244f03a2b37c15c9b9e329dff0d8f2a5fecfe8
Sofia Block <fleurfromtheblock@gmail.com>
2025-02-14 11:21:25 +0100

    initial message

---
+++ JS-Basics/js-objects-and-arrays_objects/js/index.js
@@ -0,0 +1,46 @@
+console.clear();
+
+// EXERCISE 1
+// Create an object `personWithNameAgeEmail` with the properties "name", "age" and "email".
+
+const personWithNameAgeEmail = {
+  name: "Lisa",
+  age: "14",
+  email: "lisa@gmail.com"
+};
+console.log(personWithNameAgeEmail);
+
+// EXERCISE 2
+// Set the values of `nameOfAlex` and `ageOfAlex` to match the corresponding values in the `personAlex` object.
+
+const personAlex = {
+  name: "Alex",
+  age: 24,
+};
+
+const nameOfAlex = "Change me";
+const ageOfAlex = "Change me";
+
+// EXERCISE 3
+// Change the `name` to "Alex" and `age` to "35" by updating the `personToChange`object: `person.existingProperty = newValue`.
+
+const personToChange = {
+  name: "Kim",
+  age: 24,
+};
+
+// EXERCISE 4
+// Add the properties `age` with value "5" and `breed` with value "husky" to the `petPluto` object: `pet.newProperty = newValue`.
+
+const petPluto = {
+  name: "Pluto",
+  species: "dog",
+};
+
+export {
+  personWithNameAgeEmail,
+  nameOfAlex,
+  ageOfAlex,
+  personToChange,
+  petPluto,
+};
