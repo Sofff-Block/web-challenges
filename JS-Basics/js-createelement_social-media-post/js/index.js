bb0c6ee8b962f97204bdf31b3edbc4f427e07922
Sofia Block <fleurfromtheblock@gmail.com>
2025-02-18 20:22:20 +0100

    initial commit

---
+++ JS-Basics/js-createelement_social-media-post/js/index.js
@@ -0,0 +1,20 @@
+console.clear();
+
+function handleLikeButtonClick(event) {
+  const buttonElement = event.target;
+  buttonElement.classList.toggle("post__button--liked");
+}
+
+const likeButton = document.querySelector('[data-js="like-button"]');
+likeButton.addEventListener("click", handleLikeButtonClick);
+
+// Exercise:
+// Use document.createElement() and append another social media post to the body.
+
+const newPost = document.createElement("article");
+newPost.classList.add("post");
+newPost.textContent = "=";
+document.body.append(newPost);
+
+
+
