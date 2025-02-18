204a8898c990ff193bafe4dd3ff91c415efd4566
Sofia Block <fleurfromtheblock@gmail.com>
2025-02-18 15:13:56 +0100

    changes

--- JS-Basics/js-forms_handle-form-submit/js/index.js
+++ JS-Basics/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,9 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit, (event) => {
+    event.preventDefault();
+
+const formElement = event.target;
+
