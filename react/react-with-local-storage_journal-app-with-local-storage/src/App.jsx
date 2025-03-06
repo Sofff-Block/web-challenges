abe1ac38a52117844276bcacc705da1ba7a91c89
Sofia Block <fleurfromtheblock@gmail.com>
2025-03-06 11:39:32 +0100

    install useLocalStorage

--- react/react-with-local-storage_journal-app-with-local-storage/src/App.jsx
+++ react/react-with-local-storage_journal-app-with-local-storage/src/App.jsx
@@ -5,6 +5,7 @@ import EntryForm from "./components/EntryForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
 
 const initialEntries = [
   {
@@ -37,7 +38,11 @@ const initialEntries = [
 ];
 
 function App() {
-  const [entries, setEntries] = useState(initialEntries);
+  // installed useLocalStorage, overwrite useState
+  const [entries, setEntries] = useLocalStorageState("entries", {
+    defaultValue: initialEntries,
+  });
+
   const [filter, setFilter] = useState("all"); // "all" or "favorites"
 
   function handleAddEntry(newEntry) {
